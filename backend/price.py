b996d132e571f83fb7603f9b725b26476a1b4659
Mehmet Hatip <mbhatip@gmail.com>
2021-05-06 12:19:23 -0400

    add settings to secure api token

--- backend/price.py
+++ backend/price.py
@@ -1,6 +1,12 @@
 import db
 import time
 import requests
+from configparser import ConfigParser
+
+def getToken():
+    config = ConfigParser()
+    config.read('settings.ini')
+    return config.get("DEFAULT", "token")
 
 def getPrice(id):
     id = id.lower()
@@ -8,8 +14,9 @@ def getPrice(id):
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM Price WHERE id = ?", (id,))
         row = cursor.fetchone()
+        token = getToken()
         if row is None:
-            response = requests.get(f"https://api.polygon.io/v2/aggs/ticker/{id.upper()}/prev?unadjusted=true&apiKey=pBYqVgnpohQmImag5XMM15OsMRkdhPdr")
+            response = requests.get(f"https://api.polygon.io/v2/aggs/ticker/{id.upper()}/prev?unadjusted=true&apiKey={token}")
             if not response:
                 raise Exception("API failed to get price")
             body = response.json()
@@ -20,7 +27,7 @@ def getPrice(id):
             conn.commit()
             return price
         elif time.time() - row['date'] > 86400 * 2:
-            response = requests.get(f"https://api.polygon.io/v2/aggs/ticker/{id.upper()}/prev?unadjusted=true&apiKey=pBYqVgnpohQmImag5XMM15OsMRkdhPdr")
+            response = requests.get(f"https://api.polygon.io/v2/aggs/ticker/{id.upper()}/prev?unadjusted=true&apiKey={token}")
             if not response:
                 return row['price']
             body = response.json()
